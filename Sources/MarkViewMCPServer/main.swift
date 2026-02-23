1dcced701d81285c850710a774d56fa1f74d8bcc
Paul Kang <contact@paulkang.dev>
2026-02-22 23:52:21 -0500

    Release v1.1.5

--- Sources/MarkViewMCPServer/main.swift
+++ Sources/MarkViewMCPServer/main.swift
@@ -6,7 +6,7 @@ struct MarkViewMCPServer {
     static func main() async throws {
         let server = Server(
             name: "markview",
-            version: "1.1.4",
+            version: "1.1.5",
             capabilities: .init(
                 tools: .init(listChanged: false)
             )
